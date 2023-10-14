da22024bd4d9683f07147b300029ee5f2d928acd
652110311 <118960145+652110311@users.noreply.github.com>
2023-10-14 16:43:26 +0700

    Set up routes /model for Admin

--- node/config/database.js
+++ node/config/database.js
@@ -1,40 +1,31 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-    'dii-music', // Database name
-    'postgres', // Username
-    'patty0984686684', // Password
-    {
-      host: 'localhost', // Connect to your local database
-      dialect: 'postgres' // Tell sequelize to use Postgres
-    }
-  );
-
-  async function connect() {
-    try{
-        await sequelize.authenticate();
-        console.log(
-            'connection successfully'
-        )
-    } catch(error) {
-        console.error(
-            'unable to connect',error
-        )
-    }
+  "dii-music", // Database name
+  "postgres", // Username
+  "652110311", // Password
+  {
+    host: "localhost", // Connect to your local database
+    dialect: "postgres", // Tell sequelize to use Postgres
   }
+);
 
-  async function sync() {
-    try{
-        await sequelize.sync();
-        console.log(
-            'connection successfully'
-        )
-    } catch(error) {
-        console.error(
-            'unable to connect',error
-        )
-    }
+async function connect() {
+  try {
+    await sequelize.authenticate();
+    console.log("connection successfully");
+  } catch (error) {
+    console.error("unable to connect", error);
   }
+}
 
+async function sync() {
+  try {
+    await sequelize.sync();
+    console.log("connection successfully");
+  } catch (error) {
+    console.error("unable to connect", error);
+  }
+}
 
 module.exports = { sequelize, connect, sync };
